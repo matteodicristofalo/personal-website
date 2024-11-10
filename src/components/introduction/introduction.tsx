d00aeaccffeafc1abf733902d007b40de90802de
Matteo Di Cristofalo <matteo.dicristofalo@guccidigital.com>
2024-11-10 17:10:42 +0100

    feat: add 👋 to introduction copy

--- src/components/introduction/introduction.tsx
+++ src/components/introduction/introduction.tsx
@@ -1,11 +1,19 @@
-import styles from "./introduction.module.css";
+"use client";
+
+import { useMemo } from "react";
 import { SplitTextReveal } from "@/lib/components/split-text-reveal/split-text-reveal";
+import styles from "./introduction.module.css";
 
 export function Introduction() {
+  const memoizedRevealOptions = useMemo(() => ({ stagger: 0.001 }), []);
+
   return (
     <section className={styles["introduction"]}>
       <p className={styles["introduction__paragraph"]}>
-        <SplitTextReveal text="Ciao - I'm a Frontend Developer based in Florence. Lorem ipsum dolor sit amet astate mit reles" />
+        <SplitTextReveal
+          text="Ciao 👋🏼 - I'm a Frontend Developer based in Florence. Lorem ipsum dolor sit amet astate mit reles"
+          revealOptions={memoizedRevealOptions}
+        />
       </p>
     </section>
   );
