c70528d7e2a6c1c56b965f55896cc59f082bf25f
Matteo Di Cristofalo <matteo.dicristofalo@guccidigital.com>
2024-11-16 11:28:58 +0100

    feat: contacts section reveal animation

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -1,27 +1,44 @@
+"use client";
+
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
 import { Section } from "@/components/section/section";
 import { Skill } from "./skill";
 import clsx from "clsx";
 import styles from "./skills.module.scss";
 
 export function Skills() {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end start"],
+  });
+  const height = useTransform(scrollYProgress, [0, 0.9], [50, 0]);
+
   return (
-    <Section id="skills" number="02" title="Skills">
-      <div className={clsx("grid", styles["skills"])}>
-        {skills.map((skill, i) => (
-          <div
-            className={clsx("col-12 col-lg-3", styles["skill__container"])}
-            key={i}
-          >
-            <Skill
-              number={skillNumber(i + 1)}
-              title={skill.title}
-              description={skill.description}
-              revealDelay={randomDelay()}
-            />
-          </div>
-        ))}
-      </div>
-    </Section>
+    <>
+      <Section ref={ref} id="skills" number="02" title="Skills">
+        <div className={clsx("grid", styles["skills"])}>
+          {skills.map((skill, i) => (
+            <div
+              className={clsx("col-12 col-lg-3", styles["skill__container"])}
+              key={i}
+            >
+              <Skill
+                number={skillNumber(i + 1)}
+                title={skill.title}
+                description={skill.description}
+                revealDelay={randomDelay()}
+              />
+            </div>
+          ))}
+        </div>
+      </Section>
+
+      <motion.div style={{ height }} className={styles["shape__container"]}>
+        <div className={styles["shape"]}></div>
+      </motion.div>
+    </>
   );
 }
 
