49763c0e2aaf1acfa08bc4b1340b17b3194958cb
Matteo Di Cristofalo <matteo.dicristofalo@guccidigital.com>
2024-11-16 13:30:25 +0100

    feat: skills copy

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -42,9 +42,57 @@ export function Skills() {
   );
 }
 
-const skillNumber = (index: number) => (index < 9 ? `0${index}` : `${index}`);
+const skillNumber = (index: number) => (index < 10 ? `0${index}` : `${index}`);
 
-const skills = Array(8).fill({
-  title: "Title",
-  description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-});
+const skills = [
+  {
+    title: "HTML",
+    description:
+      "Experienced in writing clean, semantic HTML for accessible and SEO-friendly web pages.",
+  },
+  {
+    title: "SCSS",
+    description:
+      "Experienced in using variables, mixins, and nesting for modular, reusable code.",
+  },
+  {
+    title: "Typescript",
+    description:
+      "Experienced in using types, improving code clarity and maintainability for scalable applications.",
+  },
+  {
+    title: "React",
+    description:
+      "Experienced in best practices with a strong understanding of lifecycles and hooks for efficient state management.",
+  },
+  {
+    title: "Next.js",
+    description:
+      "Experienced in server-side rendering and static site generation for fast, scalable web apps.",
+  },
+  {
+    title: "React testing library",
+    description:
+      "Experienced in writing unit tests with React Testing Library for reliable, maintainable code.",
+  },
+  {
+    title: "Playwright",
+    description:
+      "Experienced in end-to-end testing with Playwright for cross-browser automation, reliable tests.",
+  },
+  {
+    title: "Rollup",
+    description:
+      "Experienced in using Rollup for bundling JavaScript and building optimized npm packages",
+  },
+  {
+    title: "Docker",
+    description:
+      "Experienced in using Docker to containerize applications, ensuring consistent development, testing, and deployments.",
+  },
+  {
+    title: "Git",
+    description:
+      "Experienced in using Git from the CLI to manage version control in large codebases shared among dozens of developers.",
+  },
+];
